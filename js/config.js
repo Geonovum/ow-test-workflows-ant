b321fe1221a7731ef886fccbeec464bde246dea0
Geonovum <>
2022-08-20 10:14:32 +0000

    Update from https://github.com/Geonovum/word2markdown/commit/154a63b8eb680aa664657a948c01956b0d90de0d

---
+++ js/config.js
@@ -0,0 +1,29 @@
+var respecConfig =
+{
+  subtitle: "Informatiemodel Geluid 2022",
+  pubDomain: "ow",
+  specStatus: "GN-WV",
+  specType: "BD",
+  license: "cc-by-nd",
+  publishDate: "2022-08-20",
+  authors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "test-workflows-ant",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ow-test-workflows-ant/",
+  issueBase: "https://github.com/Geonovum/ow-test-workflows-ant/issues/",
+  edDraftURI: "https://Geonovum.github.io/ow-test-workflows-ant/",
+  generatedSubtitle: "Werkversie 20 augustus 2022"
+}
